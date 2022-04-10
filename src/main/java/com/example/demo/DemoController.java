1975a7426e4b8effe2b991dc53c0ea1e18f6ade3
Raja Biswas <raja.biswasthoughtworks.com@Rajas-MacBook-Pro.local>
2022-04-10 13:40:16 +0530

    commited in it 2 on Apr 10th

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -8,6 +8,6 @@ public class DemoController {
 
     @GetMapping("/message")
    String getMessage(){
-        return "Hello there buddy, change done in iteration 1 ! and then iteration 2";
+        return "Hello world in iteration 2";
     }
 }
